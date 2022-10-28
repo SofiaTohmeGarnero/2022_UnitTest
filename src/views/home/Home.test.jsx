2de1877fdd63bcadd4a18a104ddc09630cf13664
Sofi Tohme <sofia.tohme@globant.com>
2022-10-28 16:43:20 -0300

    [FIX] server.js to test Home view

--- src/views/home/Home.test.jsx
+++ src/views/home/Home.test.jsx
@@ -4,11 +4,9 @@ import Home from "./Home";
 describe("Home", ()=>{
     describe("when rendering default", ()=>{
         it("should show both CardContainer", async ()=>{
-            render(<Home/>)
-            screen.debug();
+            render(<Home/>);
             const CardContainerMovie = await screen.findByText("Peliculas que son tendencia");
             const CardContainerTv = await screen.findByText("Series que son tendencia");
-            screen.debug();
             await expect(CardContainerMovie).toBeInTheDocument();
             await expect(CardContainerTv).toBeInTheDocument();
         })
